733cf10c45fc811220668ab9957963fd92e559cc
roberttmello <crobertdmelo@gmail.com>
2022-11-03 11:47:24 -0300

    Add Navbar styles

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">l0g1ca booking</span>
+        <span className="logo">bimbooking</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
